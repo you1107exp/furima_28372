d1f2108ac09cf5aea83d889f677bff4fe319cd8e
you1107exp <royal.code0028@gmail.com>
2020-08-10 18:26:31 +0900

    itemsテーブルへのカラムの追加

--- db/migrate/20200810091608_create_items.rb
+++ db/migrate/20200810091608_create_items.rb
@@ -1,7 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :image
+      t.string :name
+      t.text :explanation
+      t.integer :category_id
+      t.integer :condition_id
+      t.integer :shipping_charge_id
+      t.integer :seller_prefecture_id
+      t.integer :days_until_shipping_id
+      t.integer :price
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
