349a8a77157bf1a362d857212e49cd030064cd7d
you1107exp <royal.code0028@gmail.com>
2020-08-11 11:32:34 +0900

    deviseの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_091608) do
+ActiveRecord::Schema.define(version: 2020_08_11_021607) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
