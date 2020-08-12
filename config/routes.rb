ef6fad12db3c43f825c55c5e8cc7c06a50d4ba3b
you1107exp <royal.code0028@gmail.com>
2020-08-12 14:20:58 +0900

    devise対応のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   root to: 'items#index'
   resources :items, only: :index
+  resources :registrations, only: [:new, :create]
+  resources :sessions, only: [:new, :destroy]
 end
