286b9d399f5eb3aaebcab68dd290dfbd8e2de2cb
you1107exp <royal.code0028@gmail.com>
2020-08-14 14:19:44 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :family_name, :first_name, :family_name_reading, :first_name_reading, :dob, presence: true
+  validates :nickname, :dob, presence: true
+  with_options presence: true do
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name Full-width characters"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Last name Full-width characters"}
+    validates :family_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana Full-width katakana characters"}
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana Full-width katakana characters"}
+  end
 end
