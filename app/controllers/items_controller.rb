bbc8c1341227237e9cd047e0ec5457588ea989c9
you1107exp <royal.code0028@gmail.com>
2020-08-17 00:08:36 +0900

    コントローラーの定義とマイグレーションファイルの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,14 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
   private
 
   def item_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
-  validates :name, :explanation, :category_id, :condition_id, :shipping_charge_id, :seller_prefecture_id, :days_until_shipping_id, :price, presence: true
+  validates :image, :name, :explanation, :category_id, :condition_id, :shipping_charge_id, :seller_prefecture_id, :days_until_shipping_id, :price, presence: true
 end
