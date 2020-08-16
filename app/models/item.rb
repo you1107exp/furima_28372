eb41bd758d27b1c937a5f841677e1f2200aa63d7
you1107exp <royal.code0028@gmail.com>
2020-08-16 22:50:47 +0900

    アソシエーションとプライベートメソッドの定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
-  validates :image, :name, :explanation, :category_id, :condition_id, :shipping_charge_id, :seller_prefecture_id, :days_until_shipping_id, :price, presence: true
+  belongs_to :user
+  has_one_attached :image
+  
+  validates :name, :explanation, :category_id, :condition_id, :shipping_charge_id, :seller_prefecture_id, :days_until_shipping_id, :price, presence: true
 end
