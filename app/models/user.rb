1087eff47a029c7112bd3938e3b707b7aa996fd0
you1107exp <royal.code0028@gmail.com>
2020-08-16 19:21:14 +0900

    バリデーションの記述の修正とexampleの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :dob, presence: true
+  
   with_options presence: true do
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name Full-width characters"}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Last name Full-width characters"}
-    validates :family_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana Full-width katakana characters"}
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana Full-width katakana characters"}
+    validates :nickname, :dob
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: { with: VALID_EMAIL_REGEX, message: "Email Include @" }
+
+    VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+    validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX}
+
+    VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+    validates :family_name, format: { with: VALID_NAME_REGEX, message: "Family name Full-width characters" }
+    validates :first_name, format: { with: VALID_NAME_REGEX, message: "First name Full-width characters" }
+
+    VALID_READING_REGEX = /\A[ァ-ヶー－]+\z/
+    validates :family_name_reading, format: { with: VALID_READING_REGEX, message: "Family name reading Full-width katakana characters" }
+    validates :first_name_reading, format: { with: VALID_READING_REGEX, message: "First name reading Full-width katakana characters" }
   end
 end
