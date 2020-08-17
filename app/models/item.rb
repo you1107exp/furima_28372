5645f99bff08a6f32602c804823fbdc2e867dcd6
you1107exp <royal.code0028@gmail.com>
2020-08-17 18:13:03 +0900

    価格のバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,5 @@ class Item < ApplicationRecord
   
   validates :image, :name, :explanation, :category, :condition, :shipping_charge, :seller_prefecture, :days_until_shipping, :price, presence: true
   validates :category_id, :condition_id, :shipping_charge_id, :seller_prefecture_id, :days_until_shipping_id, numericality: { other_than: 1 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
