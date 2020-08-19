286e3f7ec7c0db8dd3171412bd454ce306e127e2
you1107exp <royal.code0028@gmail.com>
2020-08-19 11:39:35 +0900

    ログイン画面へのリダイレクトの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_session, only: :new
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -20,5 +22,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :shipping_charge_id, :seller_prefecture_id, :days_until_shipping_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_session
+    unless user_signed_in?
+      redirect_to controller: 'users/sessions', action: :new
+    end
   end
 end
