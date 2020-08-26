d2c2a513faee95cf8228bf9f81088eaf1f4a335f
you1107exp <royal.code0028@gmail.com>
2020-08-26 14:36:34 +0900

    ActiveHashとモデル、マイグレーションファイルの編集

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,13 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :purchase
+
+  # validates :postal_code, :prefecture, :city, :address, :phone_number, presence: true
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture, :city, :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/}
+  end
+  validates :prefecture_id, numericality: { other_than: 1 }
 end
