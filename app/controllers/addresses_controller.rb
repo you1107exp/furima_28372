9de47f9502f1f96baff7e5f69ba2fa27a5bea867
you1107exp <royal.code0028@gmail.com>
2020-08-26 14:57:26 +0900

    addressesコントローラーへの追記

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,18 @@
 class AddressesController < ApplicationController
   def index
   end
+
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.create(address_params)
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number)
+  end
 end
