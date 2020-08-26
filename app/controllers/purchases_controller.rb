a312d0ada31c02b8c71172b7c37c752e8b9a1677
you1107exp <royal.code0028@gmail.com>
2020-08-26 18:56:35 +0900

    クレジット決済の処理

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,6 +5,29 @@ class PurchasesController < ApplicationController
   end
   
   def create
+    @purchase = Purchase.new(price: purchase_params[:price])
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_e314835f3218eb669fb310bb"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
   end
   
 end
