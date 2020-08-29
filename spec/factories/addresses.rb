0550b218418e37b1ba70b3990b231ded43b8661c
you1107exp <royal.code0028@gmail.com>
2020-08-29 17:55:19 +0900

    単体テストコードの記述

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :address do
-    
+    postal_code   {"000-0000"}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+    city          {Gimei.address.city.kanji}
+    address       {Gimei.address.town.kanji}
+    building      {Faker::Lorem.word}
+    phone_number  {Faker::Number.leading_zero_number(digits: 11)}
+    association :purchase
   end
 end
