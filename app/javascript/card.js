bc435a2b5640173847f6a940353533dfc3002ff2
you1107exp <royal.code0028@gmail.com>
2020-08-31 12:09:39 +0900

    card.js debug

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -23,6 +23,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
       Payjp.createToken(card, (status, response) => {
         if (status === 200) {
+          console.log("OK")
           // response.idでtokenが取得できます。
           const token = response.id;
           const renderDom = document.getElementById("charge-form");
