05795b4c2f0414716e13f2113ba9c191d8fbdc46
you1107exp <royal.code0028@gmail.com>
2020-08-31 18:10:05 +0900

    商品の編集・削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
   def update
     @item.update(item_params)
     if @item.save
-      redirect_to item_path(item.id)
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
